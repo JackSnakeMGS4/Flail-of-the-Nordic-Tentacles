475b7d4b5fe267e98f9a0587cf01ad93d691b424
Jaime Rivas <jaimerivas81@outlook.com>
2018-10-11 00:40:09 -0700

    bunch of constants for letter and arrow key codes; adding txt that has key codes for all keyboard keys

--- js/Input.js
+++ js/Input.js
@@ -1,7 +1,36 @@
+//Letter Keycodes
 const KEY_W = 87;
 const KEY_A = 65;
 const KEY_S = 83;
 const KEY_D = 68;
+const KEY_B = 66; 
+const KEY_C = 67; 
+const KEY_E = 69; 
+const KEY_F = 70; 
+const KEY_G = 71; 
+const KEY_H = 72; 
+const KEY_I = 73; 
+const KEY_J = 74; 
+const KEY_K = 75; 
+const KEY_L = 76; 
+const KEY_M = 77; 
+const KEY_N = 78; 
+const KEY_O = 79; 
+const KEY_P = 80; 
+const KEY_Q = 81; 
+const KEY_R = 82; 
+const KEY_T = 84; 
+const KEY_U = 85; 
+const KEY_V = 86; 
+const KEY_X = 88; 
+const KEY_Y = 89; 
+const KEY_Z = 90; 
+
+//Arrow Keycodes
+const LEFT_ARROW =  37;
+const UP_ARROW =  38;
+const RIGHT_ARROW =  39;
+const DOWN_ARROW =  40;
 
 var mouseX = 0;
 var mouseY = 0;
@@ -23,7 +52,7 @@ function updateMousePos(evt)
 	mouseX = evt.clientX - rect.left - root.scrollLeft;
 	mouseY = evt.clientY - rect.top - root.scrollTop;
 
-	//drawtext for mouse pos for debugging purposes and mayber more?
+	//draw text for mouse pos for debugging purposes? maybe more?
 }
 
 function keySet (keyEvent, viking, setTo)
